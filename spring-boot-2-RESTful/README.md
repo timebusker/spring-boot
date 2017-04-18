b09f40c18e9c067d2f2fe28ac794d464fe1777d9
timebukser <timebusker@gmail.com>
2017-04-18 16:04:32 +0800

    新增spring-boot-2-RESTful

---
+++ spring-boot-2-RESTful/README.md
@@ -0,0 +1,33 @@
+----
+## [spring-boot-2-RESTful spring boot完美支持RESTful API](https://github.com/timebusker/spring-boot/tree/master/spring-boot-2-RESTful/)
+
+### 项目阐述
+ ![image](https://github.com/timebusker/spring-boot/raw/master/static/spring-boot-2-RESTful/restful-api.png?raw=true)
+ 
+ + 理解RESTful架构
+   * [正确理解RESTful架构](http://note.youdao.com/share/?id=10ab604134fda6249816c6a718a58c93&type=note#/)
+   
+ + RESTful架构实现
+    * GET 请求
+         ![image](https://github.com/timebusker/spring-boot/raw/master/static/spring-boot-2-RESTful/get.png?raw=true)</br>
+		 ![image](https://github.com/timebusker/spring-boot/raw/master/static/spring-boot-2-RESTful/get-all.png?raw=true)</br>
+		 ![image](https://github.com/timebusker/spring-boot/raw/master/static/spring-boot-2-RESTful/get-one.png?raw=true)</br>
+		 
+    * POST 请求
+         ![image](https://github.com/timebusker/spring-boot/raw/master/static/spring-boot-2-RESTful/post.png?raw=true)</br>
+		 ![image](https://github.com/timebusker/spring-boot/raw/master/static/spring-boot-2-RESTful/add-post.png?raw=true)</br>
+		 ![image](https://github.com/timebusker/spring-boot/raw/master/static/spring-boot-2-RESTful/insert-post.png?raw=true)</br>
+	
+    * PUT 请求
+         ![image](https://github.com/timebusker/spring-boot/raw/master/static/spring-boot-2-RESTful/put.png?raw=true)</br>
+		 ![image](https://github.com/timebusker/spring-boot/raw/master/static/spring-boot-2-RESTful/update-put.png?raw=true)</br>
+		 
+    * DELETE 请求
+         ![image](https://github.com/timebusker/spring-boot/raw/master/static/spring-boot-2-RESTful/delete.png?raw=true)</br>
+		 ![image](https://github.com/timebusker/spring-boot/raw/master/static/spring-boot-2-RESTful/delete-one.png?raw=true)</br>
+
+    * HEAD 请求
+         ![image](https://github.com/timebusker/spring-boot/raw/master/static/spring-boot-2-RESTful/head.png?raw=true)</br>
+		 
+		 
+----
--- spring-boot-2-RESTful/src/main/java/cn/timebusker/web/RESTfulAPIController.java
+++ spring-boot-2-RESTful/src/main/java/cn/timebusker/web/RESTfulAPIController.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import cn.timebusker.entity.DemoInfo;
 import cn.timebusker.service.DemoInfoService;
+
+import com.alibaba.fastjson.JSON;
 
 @RestController
 public class RESTfulAPIController {
@@ -19,41 +22,60 @@ public class RESTfulAPIController {
 	@Resource
 	DemoInfoService service;
 
-	@RequestMapping(value = {"/add/","/insert"} , method=RequestMethod.POST)
+	/**
+	 * GET（SELECT）：从服务器取出资源（一项或多项）; POST（CREATE）：在服务器新建一个资源;
+	 * PUT（UPDATE）：在服务器更新资源（客户端提供改变后的完整资源）; PATCH（UPDATE）：在服务器更新资源（客户端提供改变的属性）;
+	 * DELETE（DELETE）：从服务器删除资源。
+	 */
+
+	@RequestMapping(value = { "/add" }, method = RequestMethod.POST)
 	public List<DemoInfo> addDemoInfo(@ModelAttribute DemoInfo demo) {
-		// POST请求，用来创建User
-        // 除了@ModelAttribute绑定参数之外，还可以通过@RequestParam从页面中传递参数
+		// POST请求，用来创建DemoInfo
+		// 除了@ModelAttribute绑定参数之外，还可以通过@RequestParam从页面中传递参数
+		System.out.println(JSON.toJSONString(demo));
 		return service.insertDemoInfo(demo);
 	}
 
-	@RequestMapping(value = "/{id}" , method=RequestMethod.GET)
+	@RequestMapping(value = { "/insert" }, method = RequestMethod.POST)
+	public List<DemoInfo> insertDemoInfo(@RequestParam("id") String id, @RequestParam("name") String name, @RequestParam("description") String description) {
+		// POST请求，用来创建DemoInfo
+		// 除了@ModelAttribute绑定参数之外，还可以通过@RequestParam从页面中传递参数
+		DemoInfo demo = new DemoInfo();
+		demo.setId(id);
+		demo.setDescription(description);
+		demo.setName(name);
+		System.out.println(JSON.toJSONString(demo));
+		return service.insertDemoInfo(demo);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public DemoInfo findDemoInfo(@PathVariable String id) {
-		//GET请求，用来获取信息
-		//url中的id可通过@PathVariable绑定到函数的参数中
+		// GET请求，用来获取信息
+		// url中的id可通过@PathVariable绑定到函数的参数中
 		return service.findDemoInfo(id);
 	}
 
-	@RequestMapping(value = "/update/{id}" , method=RequestMethod.PUT)
+	@RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
 	public List<DemoInfo> updateDemoInfo(@PathVariable String id) {
-		//PUT请求，用来更新信息
+		// PUT请求，用来更新信息
 		DemoInfo demo = new DemoInfo();
 		demo.setId(id);
-		demo.setDescription("我是第"+id+"ST");
-		demo.setName("TS__"+id);
+		demo.setDescription("我是第" + id + "ST");
+		demo.setName("TS__" + id);
 		return service.updateDemoInfo(demo);
 	}
 
-	@RequestMapping(value = "/del/{id}" , method=RequestMethod.DELETE)
+	@RequestMapping(value = "/del/{id}", method = RequestMethod.DELETE)
 	public List<DemoInfo> deleteDemoInfo(@PathVariable String id) {
-		//DELETE请求，用来删除信息
+		// DELETE请求，用来删除信息
 		DemoInfo demo = new DemoInfo();
 		demo.setId(id);
 		return service.deleteDemoInfo(demo);
 	}
-	
-	@RequestMapping(value = {"/all","/"} , method=RequestMethod.GET)
+
+	@RequestMapping(value = { "/all", "/" }, method = RequestMethod.GET)
 	public List<DemoInfo> findAll() {
-		//GET请求，用来获取信息
+		// GET请求，用来获取信息
 		return service.findAll();
 	}
 }
