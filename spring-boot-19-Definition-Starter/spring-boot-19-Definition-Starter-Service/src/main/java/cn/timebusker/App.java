6c02582cb20a390952c22d7e948e06a310e2633d
timebusker <timebusker@qq.com>
2017-10-10 12:07:33 +0800

    更新目录结构

---
+++ spring-boot-19-Definition-Starter/spring-boot-19-Definition-Starter-Service/src/main/java/cn/timebusker/App.java
@@ -0,0 +1,13 @@
+package cn.timebusker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
